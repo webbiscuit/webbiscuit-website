65226521926c41d6025778c95c40997cf87d69f6
Daniel <daniel@webbiscuit.co.uk>
2021-06-06 10:24:19 +0100

    Remove old deploy method

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-npm run build
-aws --profile webbiscuit s3 sync public s3://webbiscuit-site --delete
-aws --profile webbiscuit cloudfront create-invalidation --distribution-id EQJDFPS5CNFHJ --paths "/*"
