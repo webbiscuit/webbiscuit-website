c9bfde0b128dd9a03f7218acda7b7160a18e4c0d
Daniel <daniel@webbiscuit.co.uk>
2022-09-18 09:58:27 +0100

    Moving to barebones

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,11 @@
 import * as React from 'react'
-import Layout from '../components/layout'
-import { StaticImage } from 'gatsby-plugin-image'
 import Seo from '../components/seo'
 
 const IndexPage = () => {
-  return (
-    <Layout pageTitle="Home Page">
-      <noscript>Ah you have no js</noscript>
-      <p>I'm making this by following the Gatsby Tutorial.</p>
-      <StaticImage
-        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
-      />
-    </Layout>
-  )
-}
+    return (
+      <p>Yo</p>
+    )
+  }
 
 export const Head = () => <Seo title="Home Page" />
 
