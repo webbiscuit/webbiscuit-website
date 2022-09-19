69112ec6b9b1d2ef7ef4dae3ab37532694c87ead
Daniel <daniel@webbiscuit.co.uk>
2022-09-19 19:26:13 +0100

    Present blogs a little nicer

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,7 +2,7 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `Web Biscuit Website`,
+    title: `I am Web Biscuit`,
     siteUrl: `https://www.webbiscuit.co.uk`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,24 +1,46 @@
-import { Text, Heading, Box } from '@chakra-ui/react'
-import { graphql, PageProps } from 'gatsby'
+import { Text, Heading, Box, VStack, HStack } from '@chakra-ui/react'
+import { graphql, Link, PageProps } from 'gatsby'
+import { GatsbyImage, getImage, IGatsbyImageData, ImageDataLike } from 'gatsby-plugin-image'
 import * as React from 'react'
 import Layout from '../../components/layout'
 import Seo from '../../components/seo'
 
-const BlogPreview = ({title, date, description}: {title?: string, date?: string, description?: string}) => {
+const BlogLatestPreview = ({title, date, description}: {title?: string, date?: string, description?: string}) => {
+  return (
+    <Box as="article" borderStyle={'solid'} borderColor="yellow.900" borderWidth={4} padding={2} mb={4}>
+      {/* <Text>Latest Post!</Text> */}
+      <Heading size="xl" fontWeight={600}>{title}</Heading>
+      {date && <Text as="time" fontSize={"xs"} dateTime={date}>{date}</Text>}
+      <Text>{description}</Text>
+    </Box>
+  )
+}
+
+const BlogPreview = ({title, date, description, image, image_alt, slug}: 
+    {title?: string, date?: string, description?: string, image?: IGatsbyImageData, image_alt?: string, slug?: string}) => {
     return (
       <Box as="article" borderStyle={'solid'} borderColor="yellow.900" borderWidth={4} borderBottom={0} padding={2}
         _last={{
           borderBottom: "4px"
-        }}>
-        <Heading size="xl" fontWeight={600}>{title}</Heading>
-        {date && <Text as="time" fontSize={"xs"} dateTime={date}>{date}</Text>}
-        <Text>{description}</Text>
+        }}>  
+        <HStack spacing='24px'>
+          
+          <Link to={`/blog${slug}`}><Heading size="xl" fontWeight={600}>{title}</Heading></Link>
+
+          <VStack>
+            {date && <Text as="time" fontSize={"xs"} dateTime={date}>{date}</Text>}
+            <Text>{description}</Text>
+          </VStack>
+          {/* {image && <GatsbyImage 
+            image={image}
+            alt={image_alt ?? "A blog image"}
+          />} */}
+        </HStack>
       </Box>
     )
 }
 
 const BlogPage = ({data} : PageProps<Queries.BlogPageQuery>) => {
-    
     return (
       <Layout>
         <Box as="section">
@@ -28,12 +50,25 @@ const BlogPage = ({data} : PageProps<Queries.BlogPageQuery>) => {
           </Box>
           <Box boxShadow='2xl' p='6' rounded='md' bg='yellow.50'>
           {
-            data.allMdx.nodes.map((node) => {
-              return <BlogPreview 
-                key={node.id}
-                title={node?.frontmatter?.title ?? undefined} 
-                date={node?.frontmatter?.date ?? undefined}
-                description={node?.frontmatter?.description ?? undefined} />
+            data.allMdx.nodes.map((node, ix, arr) => {
+              const image = node.frontmatter?.hero_image ? getImage(node.frontmatter?.hero_image as ImageDataLike) : undefined;
+
+              if (ix === 110) {
+                return <BlogLatestPreview 
+                  key={node.id}
+                  title={node.frontmatter?.title ?? undefined} 
+                  date={node.frontmatter?.date ?? undefined}
+                  description={node.frontmatter?.description ?? undefined} />
+              }
+              else {
+                return <BlogPreview 
+                  key={node.id}
+                  image={image}
+                  title={node.frontmatter?.title ?? undefined} 
+                  date={node.frontmatter?.date ?? undefined}
+                  description={node.frontmatter?.description ?? undefined} 
+                  slug={node.fields?.slug ?? undefined} />
+              }
             })
           }
           </Box>
@@ -50,12 +85,20 @@ export const query = graphql`
           date(formatString: "Do MMM YYYY")
           title
           description
+          hero_image_alt
+          hero_image {
+            childImageSharp {
+              gatsbyImageData(
+                height: 100
+              )
+            }
+          }
         }
         fields {
           slug
         }
         id
-        excerpt
+        excerpt   
       }
     }
   }`
