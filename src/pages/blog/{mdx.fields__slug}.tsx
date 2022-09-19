4fffe65ff67c9def8bc10efc37be0fa3efb58065
Daniel <daniel@webbiscuit.co.uk>
2022-09-19 19:36:17 +0100

    Added slug blog page

---
+++ src/pages/blog/{mdx.fields__slug}.tsx
@@ -0,0 +1,65 @@
+import * as React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../../components/layout'
+import Seo from '../../components/seo'
+import { Text, Heading, Box, VStack, HStack } from '@chakra-ui/react'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+
+const BlogPost = ({data, children}) => {
+  const image = getImage(data.mdx.frontmatter.hero_image)
+
+  return (
+    <Layout>
+      <Heading size="xl" fontWeight={600}>{data.mdx.frontmatter.title}</Heading>
+     {image && <GatsbyImage 
+       image={image}
+       alt={data.mdx.frontmatter.hero_image_alt}
+     />}
+      {children}
+    </Layout>
+  )
+  // const image = getImage(data.mdx.frontmatter.hero_image)
+
+  // console.log(data.mdx.frontmatter.hero_image);
+
+  // return (
+  //   <Layout pageTitle={data.mdx.frontmatter.title}>
+  //     <p>Posted: {data.mdx.frontmatter.date}</p>
+  //     <GatsbyImage 
+  //       image={image}
+  //       alt={data.mdx.frontmatter.hero_image_alt}
+  //     />
+  //     <p>
+  //       {data.mdx.frontmatter.hero_image_alt} - 
+  //       Photo Credit:{" "}
+  //       <a href={data.mdx.frontmatter.hero_image_credit_link}>
+  //         {data.mdx.frontmatter.hero_image_credit}
+  //       </a>
+  //     </p>
+  //     {children}
+  //   </Layout>
+}
+
+export const query = graphql`
+  query ($id: String) {
+    mdx(id: {eq: $id}) {
+      frontmatter {
+        title
+        description
+        date(formatString: "MMMM D, YYYY")
+        hero_image_alt
+        hero_image_credit
+        hero_image_credit_link
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
+      }
+    }
+  }
+`
+
+export const Head = ({data}) => <Seo title={data.mdx.frontmatter.title} description={data.mdx.frontmatter.description}/>
+
+export default BlogPost
