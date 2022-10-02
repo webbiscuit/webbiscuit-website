5122574fd5851d9e0cab04e8ef84c879e608039a
Daniel <daniel@webbiscuit.co.uk>
2022-10-02 13:33:14 +0100

    Add titles/descriptions to some pages

--- src/templates/pages.tsx
+++ src/templates/pages.tsx
@@ -4,8 +4,16 @@ import { MDXProvider } from "@mdx-js/react";
 import { Link } from "gatsby";
 import { MdxWrapper } from "../components/mdx-wrapper";
 import Layout from "../components/layout";
+import Seo from "../components/seo";
 
-// const shortcodes = { Link }; // Provide common components here
+type PageQuery = {
+  readonly mdx: {
+    readonly frontmatter: {
+      readonly title: string | null;
+      readonly description: string | null;
+    } | null;
+  } | null;
+};
 
 export default function PageTemplate({ data, children }) {
   return (
@@ -22,7 +30,15 @@ export const query = graphql`
     mdx(id: { eq: $id }) {
       frontmatter {
         title
+        description
       }
     }
   }
 `;
+
+export const Head = ({ data }: { data: PageQuery }) => (
+  <Seo
+    title={data.mdx?.frontmatter?.title ?? "Untitled"}
+    description={data.mdx?.frontmatter?.description ?? undefined}
+  />
+);
